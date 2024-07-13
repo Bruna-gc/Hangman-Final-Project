97093ce74bc25a65b508a93db9144861270f8592
Bruna-gc <130918862+Bruna-gc@users.noreply.github.com>
2024-07-13 18:03:15 -0400

    breezypythongui_interface.py

--- breezypythongui_interface.py
+++ breezypythongui_interface.py
@@ -1,4 +1,5 @@
 from breezypythongui import EasyFrame
+from tkinter import messagebox
 
 from tkinter import PhotoImage
 import tkinter as tk
@@ -8,51 +9,55 @@ import tkinter as tk
 class Hangman(EasyFrame):
     def __init__(self):
         EasyFrame.__init__(self, title="Hangman Frame with breezypythongui", width="450", height="600")
-        self.image = PhotoImage(file="images/lineNoBG.png")
 
         # create list for letters in word
         self.list_letters = [i for i in "appartment"]
+        
+        #header panel
+        headerPanel = self.addPanel(row=0, column=0, columnspan=3, background="gray")
+        headerPanel.addButton(text="Exit", row=0, column=0, command=self.quit)
+        headerPanel.addLabel(text="Title will go here", row=0, column=1, background="gray", sticky='W')
 
-        # self.list_letters = []
-        # for i in range(len("school")):
-        #     if "school"[i] == "school"[i-1]:
-        #         self.list_letters.append("school"[i]+str(i))
-        #     self.list_letters.append("school"[i])
+        # used words
+        self.used_letters_list = set()
+        self.used_letters = self.addLabel(text="Used word will go here", row=2, column=0, sticky="NEWS")
 
-        # print(self.list_letters)
-        wordPanel = self.addPanel(row=2, column=0, background="beige")
+        #hangman panel
+        hangmanPanel = self.addPanel(row=4, column=0, columnspan=3, background="light blue")
+        hangmanPanel.addLabel(text="possible hangman will go here", row=0, column=0, sticky='NEWS', background="light blue")
 
-        # create dict for key = letter / value = image dash 
+        #word panel
+        wordPanel = self.addPanel(row=6, column=0, background="beige", columnspan=3)
+
+        # create dict for key = text / value = letter, otherwise I couldn't creat the same key for double letters
         self.word_dict = {}
         for i in range(len(self.list_letters)):
             value = self.list_letters[i]
-            key = wordPanel.addLabel(text="_",row=2, column=i, sticky='NWES', font="Arial", background="beige")
+            key = wordPanel.addLabel(text="_",row=0, column=i, sticky='NWES', font="Arial", background="beige")
             self.word_dict[key] = value
 
-        # print(self.word_dict)
+        self.letter_input = self.addTextField(text="", row=8, column=1, sticky='NW')
 
-        self.letter_input = self.addTextField(text="", row=3, column=0)
+        self.addButton(text="Enter", row=8, column=2, command=self.onSubmit)
 
-        self.addLabel(text="This is a label", row=1, column=0, sticky="NEWS")
-        self.addButton(text="Button", row=4, column=0, command=self.onSubmit)
 
     def onSubmit(self):
         letter = self.letter_input.getText()
-        # print(self.word_dict.items())
-        items = [i for i in self.word_dict.items()]
+        if len(letter) != 1:
+            messagebox.showerror("Invalid letter", "enter only one letter")
+            return
 
-        for (key, value) in items:
-            print(key, value)
-            if letter in (key,value)[-1]:
+        # for loop bcause of double letters in words
+        for (key, value) in self.word_dict.items():
+            if letter.lower() in (key,value)[-1]:
                 key["text"] = value
-                # self.word_dict[new_key] = self.word_dict.pop(key)
-
-        # if letter in self.list_letters:
-        #     # self.word_dict[letter]["text"] = letter
-        #     # print(self.word_dict.values())
-        #     pass
             else:
                 print('Not in word')
+                self.used_letters_list.add(letter+",")
+                # assign text to list of words (for better display)
+                self.used_letters["text"] = list(self.used_letters_list)
+                self.letter_input.setText("")
+                return
             self.letter_input.setText("")
 
 
